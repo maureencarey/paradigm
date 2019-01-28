49912b2969a6744f752d800c6df101c9ae50c8d2
maureencarey <maureen.carey@mac.com>
2019-01-28 17:09:44 -0500

    (untested so far) added annotations to universal and iPfal18

--- run_these/update_universal_reaction_set.py
+++ run_these/update_universal_reaction_set.py
@@ -15,8 +15,10 @@ logger = logging.getLogger(__name__)
 
 logger.info('begin: update of universal reaction set')
 
-data_path = "/home/mac9jc/paradigm/data/"
-model_path = "/home/mac9jc/paradigm/models/"
+#data_path = "/home/mac9jc/paradigm/data/"
+#model_path = "/home/mac9jc/paradigm/models/"
+data_path = "/Users/maureencarey/local_documents/work/comparative_parasite_models/paradigm/data"
+model_path = "/Users/maureencarey/local_documents/work/comparative_parasite_models/paradigm/models"
 
 os.chdir(model_path)
 universal = cobra.io.load_json_model('universal_model_oct26_2018.json')
@@ -32,19 +34,19 @@ def met_ids_without_comp(met_id):
     # only one id listed
     # print list of metabolites without the compartment associated
     # this needs to be updated if you have different compartments than those listed below
-
-    if m.id.endswith('_c') or m.id.endswith('_e') or m.id.endswith('_f') or \
-    m.id.endswith('_g') or m.id.endswith('_h') or m.id.endswith('_i') or \
-    m.id.endswith('_l') or m.id.endswith('_m') or m.id.endswith('_n') or \
-    m.id.endswith('_p') or m.id.endswith('_r') or m.id.endswith('_s') or \
-    m.id.endswith('_u') or m.id.endswith('_v') or m.id.endswith('_x'):
-        id_withou_c = m.id[:-2]
-    elif m.id.endswith('_cx') or m.id.endswith('_um') or m.id.endswith('_im') \
-    or m.id.endswith('_ap') or m.id.endswith('_fv'):
-        id_withou_c = m.id[:-3]
+    m = met_id
+    if m.endswith('_c') or m.endswith('_e') or m.endswith('_f') or \
+    m.endswith('_g') or m.endswith('_h') or m.endswith('_i') or \
+    m.endswith('_l') or m.endswith('_m') or m.endswith('_n') or \
+    m.endswith('_p') or m.endswith('_r') or m.endswith('_s') or \
+    m.endswith('_u') or m.endswith('_v') or m.endswith('_x'):
+        id_withou_c = m[:-2]
+    elif m.endswith('_cx') or m.endswith('_um') or m.endswith('_im') \
+    or m.endswith('_ap') or m.endswith('_fv'):
+        id_withou_c = m[:-3]
     else:
         print('unknown compartment')
-        print(m.id)
+        print(m)
         id_withou_c = ''
 
     return(id_withou_c)
@@ -56,58 +58,111 @@ logger.info('removed extra biomass reactions')
 
 # add full met info
 for met in universal.metabolites:
-    met_id = met_ids_without_comp(met.id)
-    m = requests.get('http://bigg.ucsd.edu/api/v2/universal/metabolites/{}'.format(met_id)))
-    x = m.json()
-    met.name = x['name']
-    met.notes = x['database_links']
-    met.formula = x['formulae'][0]
-    met.charge = x['charges'][0]
-
-# add full reaction info
-need_info = list()
-for rxn in universal.reactions:
-    rxn_id = rxn.id
-    m = requests.get('http://bigg.ucsd.edu/api/v2/universal/reactions/{}'.format(rxn_id))
-    x = m.json()
-    rxn.name = x['name']
-    rxn.reaction = x['reaction_string']
-    rxn.notes = x['database_links']
-    # also have info on x['pseudoreaction']
-
-# SAVE
-os.chdir(model_path)
-cobra.io.save_json_model('universal_model_updated.json')
-
-rxn_list = [r.id for r in universal.reactions]
-met_list = [m.id for m in universal.metabolites]
-
-# same for Pf model
-os.chdir(model_path)
-model = cobra.io.load_json_model('iPfal18.json')
-
-# add full met info
-for met in model.metabolites:
+    
     met_id = met_ids_without_comp(met.id)
     
-    if met_id+'_c' in met_list:
-        m = requests.get('http://bigg.ucsd.edu/api/v2/universal/metabolites/{}'.format(met_id+'_c')))
-        x = m.json()
+    m = requests.get('http://bigg.ucsd.edu/api/v2/universal/metabolites/{}'.format(met_id))
+    x = m.json()
+    
+    if met.name == '':
         met.name = x['name']
-        met.notes = x['database_links']
+    
+    # fix current reaction.annotations field that is in list form
+    # this fix will allow universal to be written as as a xml file, not just json
+    # however save the info just in case it is not duplicated elsewhere
+
+    # rxn.notes are currently {'original_bigg_id':[id_string]}
+    annot_list = met.annotation
+    if isinstance(annot_list, list):
+        if len(annot_list) >1:
+            for sub_list in annot_list:
+                if sub_list[0] not in met.notes.keys():
+                    met.notes[sub_list[0]] = sub_list[1]
+                else:
+                    met.notes[sub_list[0]] = [met.notes[sub_list[0]]].append(sub_list[1])
+        else:
+            met.notes[sub_list[0]] = sub_list[0]
+
+    met.annotations = x['database_links']
+    if x['formulae'] != [] and len(x['formulae'])>0:
         met.formula = x['formulae'][0]
+    if x['charges'] != [] and len(x['charges'])>0:
         met.charge = x['charges'][0]
 
 # add full reaction info
 need_info = list()
 for rxn in universal.reactions:
-    rxn_id = rxn.id
     
-    if rxn_id in rxn_list:
-        m = requests.get('http://bigg.ucsd.edu/api/v2/universal/reactions/{}'.format(rxn_id))
-        x = m.json()
+    rxn_id = rxn.id
+    m = requests.get('http://bigg.ucsd.edu/api/v2/universal/reactions/{}'.format(rxn_id))
+    x = m.json()
+    
+    if rxn.name == '':
         rxn.name = x['name']
-        rxn.notes = x['database_links']
-# also have info on x['pseudoreaction']
 
-save_json_model(model,  'iPfal18.json')
+    # fix current reaction.annotations field that is in list form
+    # this fix will allow universal to be written as as a xml file, not just json
+    # however save the info just in case it is not duplicated elsewhere
+
+    # rxn.notes are currently {'original_bigg_id':[id_string]}
+    annot_list = rxn.annotation
+    if isinstance(annot_list, list):
+        if len(annot_list) >1:
+            for sub_list in annot_list:
+                if sub_list[0] not in rxn.notes.keys():
+                    rxn.notes[sub_list[0]] = sub_list[1]
+                else:
+                    rxn.notes[sub_list[0]] = [rxn.notes[sub_list[0]]].append(sub_list[1])
+        else:
+            rxn.notes[sub_list[0]] = sub_list[0]
+    rxn.reaction = x['reaction_string']
+    rxn.annotation = x['database_links']
+    # also have info on x['pseudoreaction']
+    # COULD ADD SBO TERM HERE
+
+# SAVE
+os.chdir(model_path)
+cobra.io.save_json_model(universal, 'universal_model_updated.json')
+cobra.io.write_sbml_model(universal, 'universal_model_updated.xml')
+
+#rxn_list = [r.id for r in universal.reactions]
+#met_list = [m.id for m in universal.metabolites]
+#
+## same for Pf model
+#os.chdir(model_path)
+#model = cobra.io.load_json_model('iPfal18.json')
+#
+## add full met info
+#for met in model.metabolites:
+#    met_id = met_ids_without_comp(met.id)
+#
+#    if met_id+'_c' in met_list:
+#        m = requests.get('http://bigg.ucsd.edu/api/v2/universal/metabolites/{}'.format(met_id+'_c'))
+#        x = m.json()
+#        if met.name == '':
+#            met.name = x['name']
+#        if met.notes = '':
+#            met.notes = met.annotation
+#        met.annotation = x['database_links'] # check if compartment info
+        # if x['formulae'] is not []:
+    #        met.formula = x['formulae'][0]
+#        if x['charges'] is not []:
+    #        met.charge = x['charges'][0]
+#
+## add full reaction info
+#need_info = list()
+#for rxn in model.reactions:
+#    rxn_id = rxn.id
+#
+#    if rxn_id in rxn_list:
+#        m = requests.get('http://bigg.ucsd.edu/api/v2/universal/reactions/{}'.format(rxn_id))
+#        x = m.json()
+#        if rxn.name == '':
+#            rxn.name = x['name']
+#        if rxn.notes = '':
+#            rxn.notes = rxn.annotation
+#        rxn.annotation = x['database_links'] # check if compartment info
+## also have info on x['pseudoreaction']
+#
+#save_json_model(model,  'iPfal18_updated.json')
+
