1833703fc83ccd35671d83fd57dee4e73d7ba988
maureencarey <maureen.carey@mac.com>
2019-07-04 16:26:25 -0400

    remove results for fresh troubleshoot

--- slurm_scripts/pipeline_slurm_step1a.sh
+++ slurm_scripts/pipeline_slurm_step1a.sh
@@ -4,38 +4,38 @@
 # DOWNLOAD GENOMES
 cd ~/paradigm
 echo "changed directory"
-# Rscript ./data_acquistion/step_0_download_all_eupathDB_release41.r
+Rscript ./data_acquistion/step_0_download_all_eupathDB_release44.r
 echo "downloaded models"
 
 # PREP FOR ANNOTATE GENOMES
 cd ~/paradigm/data
-# get database sequence files to make protein database for annotating sequences
-# wget -O bigg_proteins.fasta 'https://github.com/cdanielmachado/carveme/raw/master/carveme/data/input/bigg_proteins.faa'
-# diamond makedb --in bigg_proteins.fasta -d bigg_proteins_diamond
-# wget -O aa_seqs_OrthoMCL_5.fasta 'http://orthomcl.org/common/downloads/release-5/aa_seqs_OrthoMCL-5.fasta'
-# diamond makedb --in aa_seqs_OrthoMCL_5.fasta -d orthoMCL_proteins_diamond
+get database sequence files to make protein database for annotating sequences
+wget -O bigg_proteins.fasta 'https://github.com/cdanielmachado/carveme/raw/master/carveme/data/input/bigg_proteins.faa'
+diamond makedb --in bigg_proteins.fasta -d bigg_proteins_diamond
+wget -O aa_seqs_OrthoMCL_5.fasta 'http://orthomcl.org/common/downloads/release-5/aa_seqs_OrthoMCL-5.fasta'
+diamond makedb --in aa_seqs_OrthoMCL_5.fasta -d orthoMCL_proteins_diamond
 # get other useful files for downstream work
-# wget -O bigg_gprs.csv.gz 'https://github.com/cdanielmachado/carveme/raw/master/carveme/data/generated/bigg_gprs.csv.gz'
-# gunzip bigg_gprs.csv.gz
-# wget -O bigg_metabolites.txt 'http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt'
-# wget -O bigg_reactions.txt 'http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt'
-# #wget -O metanetx_chem_prop.tsv 'https://www.metanetx.org/cgi-bin/mnxget/mnxref/chem_prop.tsv'
-# #must uncomment out header of metanetx_chem_prop
+wget -O bigg_gprs.csv.gz 'https://github.com/cdanielmachado/carveme/raw/master/carveme/data/generated/bigg_gprs.csv.gz'
+gunzip bigg_gprs.csv.gz
+wget -O bigg_metabolites.txt 'http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt'
+wget -O bigg_reactions.txt 'http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt'
+#wget -O metanetx_chem_prop.tsv 'https://www.metanetx.org/cgi-bin/mnxget/mnxref/chem_prop.tsv'
+#must uncomment out header of metanetx_chem_prop
 echo "made annotation databases and other necessary files"
 
 # ANNOTATE GENOMES
 cd ~/paradigm/data/genomes/protein
-for filename in ./*_annotatedProteins_March2019.fasta; do
-#echo "$filename"
-#echo "${filename:2:${#filename}-34}"
-diamond blastp -d ~/paradigm/data/bigg_proteins_diamond -q $filename -o "${filename:2:${#filename}-36}_BiGG.tsv"
+for filename in ./*_annotatedProteins.fasta; do
+echo "$filename"
+echo "${filename:2:${#filename}-24}"
+diamond blastp -d ~/paradigm/data/bigg_proteins_diamond -q $filename -o "${filename:2:${#filename}-26}_BiGG.tsv"
 done
 echo "diamond annotation against BiGG done"
 mv ./*_BiGG.tsv ~/paradigm/data/diamond_output_BiGG
 
 cd ~/paradigm/data/genomes/protein
-for filename in ./*_annotatedProteins_March2019.fasta; do
-diamond blastp -d ~/paradigm/data/orthoMCL_proteins_diamond -q $filename -o "${filename:2:${#filename}-36}_orthoMCL.tsv"
+for filename in ./*_annotatedProteins.fasta; do
+diamond blastp -d ~/paradigm/data/orthoMCL_proteins_diamond -q $filename -o "${filename:2:${#filename}-26}_orthoMCL.tsv"
 done
 mv ./*_orthoMCL.tsv ~/paradigm/data/diamond_output_orthoMCL
 echo "diamond annotations complete"
