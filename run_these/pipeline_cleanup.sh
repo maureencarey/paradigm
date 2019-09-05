b90e915d0b34e5349897a0b4fa9bc6cef47f8fc4
maureencarey <maureen.carey@mac.com>
2019-09-05 09:37:08 -0400

    added module purge

--- run_these/pipeline_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# home/mac9jc/paradigm
-
-# CHECK ALL LOGS FOR INFEASIBLE STEPS
-cd ~/paradigm/model_generation_logs # paradigm directory
-for filename in step*.log; do
-echo "$filename"
-python3 ../slurm_scripts/check_logs.py $filename
-done
